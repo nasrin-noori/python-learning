9ab5565e6c83b0261e992ae74c500a41afb2919c
nasrin-noori <133895003+nasrin-noori@users.noreply.github.com>
2023-06-01 13:14:30 +0330

    fiboiterative.py

---
+++ fiboiterative.py
@@ -0,0 +1,17 @@
+def fibo(len): 
+    if len<=0:
+        return 0
+    elif len==1:
+      return 1
+    else:
+      num_1=0
+      num_2=1
+      for i in range(2,len+1):
+        result=num_1+num_2
+        num_1=num_2
+        num_2=result
+    return(result)
+assert fibo(2)==1
+assert fibo(2)+fibo(3)==fibo(4)
+   
+    
