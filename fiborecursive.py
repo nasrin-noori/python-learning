f0cbfd7db440ad727a9145930f3c595ad4761454
nasrin-noori <133895003+nasrin-noori@users.noreply.github.com>
2023-06-01 14:10:29 +0330

    fiborecursive.py

---
+++ fiborecursive.py
@@ -0,0 +1,10 @@
+def fibo(len):
+  if len<=o:
+    return 0
+  elif len==1:
+    return 1
+  else:
+    retrun(fibo(len)=fibo(len-1)+fibo(len-2))
+assert fibo(2)==1
+assert fibo(2)+fibo(3)==fibo(4)
+    
