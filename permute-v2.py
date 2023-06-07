68b8a0c21d444c6ca4414de8c3ee9f9f21c6b5b6
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2023-06-07 18:50:44 +0330

    Update permute-v2.py
    new todo

--- permute-v2.py
+++ permute-v2.py
@@ -1,8 +1,7 @@
-t=input('enter longer string: ')
-s=input('enter shorter string: ')
+
 def permute(data,i,length):
-    result=[]
-    if i==length:
+    result = []
+    if i == length:
         result.append(''.join(data))
     else:
         for j in range(i,length):
@@ -21,6 +20,14 @@ def asses_logic(export, search_string):
             indicator=1
     if indicator!=1:
         print('NO permutation found')
-result = permute(list(s), 0, len(s))
-asses_logic(result,t)
-#this function is o(n!*length of longer string)
+
+        
+if __name__ = '__main__':
+    t = input('enter longer string: ')
+    s = input('enter shorter string: ')
+    result = permute(list(s), 0, len(s))
+    asses_logic(result,t)
+    #this function is o(n!*length of longer string)
+    # TODO: optimize the algorithm to reach o(nm) where n is the length of the short and the m is the length of the long string
+    # aaab vs abaa ? {a: 3, b: 1}
+    # find abab in aabbbaaaba
