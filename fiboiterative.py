82a9b88ff0b7865193e404e9160ae0dd94d3ea9d
nasrin-noori <133895003+nasrin-noori@users.noreply.github.com>
2023-06-10 16:54:24 +0330

    Update fiboiterative.py

--- fiboiterative.py
+++ fiboiterative.py
@@ -16,3 +16,17 @@ assert fibo(2)+fibo(3)==fibo(4)
    
     
 # TODO: write it using Generators (keyword yield vs return)
+
+def fibo_generator(n):
+    num_1=0
+    num_2=1
+    yield num_1
+    yield num_2
+    for i in range(2, n+1):
+        result=num_1+num_2
+        num_1=num_2
+        num_2=result
+        yield result
+fibo_sequence=fibo_generator(int(input('enter n :')))
+for num in fibo_sequence:
+    print(num)
